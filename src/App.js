3c527835c084f5f77e47c2b2276050449be58416
chuongvu2001 <chuongvu2806@gmail.com>
2021-03-18 08:39:30 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { Header } from './components/Header';
+import { Banner } from './components/Banner';
+import { Albums } from './components/Albums';
+import { Footer } from './components/Footer';
 function App() {
+  const myName = "Chun dep trai";
+
+  const myAge = (a, b) => {
+    return a + b;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+       <Header />
+        <main>
+          <Banner />
+          <Albums />
+        </main>
+       <Footer />
+      </div>
 
+    </div>
+  )
+}
 export default App;
